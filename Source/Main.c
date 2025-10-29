1c72366def6f256fc3438737e7eee8eaa1131eed
alihakankurt <kurtalihakan@outlook.com>
2025-10-29 22:24:02 +0300

    Update project language from C++23 to C11

--- Source/Main.c
+++ Source/Main.c
@@ -1,14 +1,14 @@
 #include <SDL3/SDL.h>
 
 const char* Title = "Flabee";
-constexpr int WindowWidth = 1280;
-constexpr int WindowHeight = 720;
+int WindowWidth = 1280;
+int WindowHeight = 720;
 
-int main(int argc, const char* argv[])
+int main(void)
 {
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Initialization Failure", "Unable to initialize SDL", nullptr);
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Initialization Failure", "Unable to initialize SDL", 0);
         return 1;
     }
 
@@ -16,7 +16,7 @@ int main(int argc, const char* argv[])
     SDL_Renderer* renderer;
     if (!SDL_CreateWindowAndRenderer(Title, WindowWidth, WindowHeight, SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Window/Renderer Creation Failure", "Unable to create window or renderer", nullptr);
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Window/Renderer Creation Failure", "Unable to create window or renderer", 0);
         return 1;
     }
 
@@ -57,7 +57,7 @@ int main(int argc, const char* argv[])
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        const SDL_FRect rect{positionX,positionY,30,30};
+        const SDL_FRect rect = {positionX, positionY, 30, 30};
         SDL_SetRenderDrawColor(renderer, 160, 160, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
 
